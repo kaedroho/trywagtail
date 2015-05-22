ea770b8e16218e035d429f484068bc968ec26e7f
Karl Hobley <karlhobley10@gmail.com>
2015-05-22 14:40:15 +0100

    Initial commit

---
+++ trywagtail/core/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'trywagtail.core.apps.TryWagtailAppConfig'
---
+++ trywagtail/core/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TryWagtailAppConfig(AppConfig):
+    name = 'trywagtail.core'
+    label = 'trywagtail'
+    verbose_name = "Try Wagtail"
